62fd23615dd3d80451e39e425fe386c6b77f04d5
chenshasha <chenshasha@zhiyitech.cn>
2020-08-03 14:36:31 +0800

    add

--- JavaScript/async&&await/demo.js
+++ JavaScript/async&&await/demo.js
@@ -26,3 +26,35 @@ async function startLightOn(){
     await lightOn(2000,'yellow');
     await lightOn(3000,'green');
 }
+
+/* 假设有一个业务场景2需要实现：
+    我先去超市买菜
+    用超市买回来的菜做饭
+    将做好的饭菜送到ywx单位
+*/
+function buyFood(){
+    return new Promise(resolve=>{
+        console.log('买菜')
+        setTimeout(() => {
+            resolve()            
+        }, 1000);
+    })
+}
+function makeFood(){
+    return new Promise(resolve=>{
+        console.log('做饭')
+        setTimeout(() => {
+            resolve()            
+        }, 2000);
+    })
+}
+function sendFood(){
+    return new Promise(resolve=>{
+        console.log('做饭')
+        setTimeout(() => {
+            resolve()            
+        }, 3000);
+    })
+}
+buyFood().then(()=>makeFood()).then(()=>sendFood()).then(()=>{console.log('ok')})
+
